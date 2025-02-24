744f8d32295a63aea228127ff6b57a2e76f20303
Matheus-Cash <149600579+Matheus-Cash@users.noreply.github.com>
2025-02-24 19:13:47 +0000

    234

--- media.js
+++ media.js
@@ -1,7 +1,6 @@
 let notas = [10, 6.5, 7.5];
 
-notas.pop(10);
-notas.pop(7.5);
+
 
 let media = (notas[0] + notas{ 1} + notas[2] + notas[3]); / notas.length;
 console.log
--- remover-nota.js
+++ remover-nota.js
@@ -1,5 +1,7 @@
 let notas = [10, 6.5, 7.5];
 
+notas.pop(10);
+notas.pop(7.5);
 
 let media = (notas[0] + notas{ 1} + notas[2] + notas[3]); / notas.length
 console.log
