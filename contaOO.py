5c61849dbeeed9d4e6e5f35f9187c0da02511b68
Matheus-Cash <149600579+Matheus-Cash@users.noreply.github.com>
2025-03-27 14:11:43 -0300

    214

--- contaOO.py
+++ contaOO.py
@@ -3,3 +3,6 @@ class conta:
 
     def __int__(self, numero,titular,saldo,limite ):
         self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
