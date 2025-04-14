1a4ba56ddb323fdcf0f9aa74171b9a2b0cc57a80
Matheus-Cash <149600579+Matheus-Cash@users.noreply.github.com>
2025-04-14 16:15:00 -0300

    124

---
+++ streaming.py
@@ -0,0 +1,30 @@
+class Filme:
+    def __init__(self, nome, ano , duracao, imdb):
+        self.__nome = nome
+        self.ano = ano
+        self.duracao = duracao
+        self.imdb = imdb
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    def def_likes(self):
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @property
+    def imdb(self):
+        return self.__imdb
+
+    @imdb.setter
+    def imdb(self, imdb):
+        self.__imdb = imdb
+
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
